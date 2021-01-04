962740d0c31fb6fcf80b594ef6300f4c6b6fafe3
Aurélio L. Andrade <aurelio@sistemico.com.br>
2021-01-04 18:09:27 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -27,3 +27,7 @@ INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Text n
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://c.pxhere.com/images/22/ac/1d5d2173b4dc18c4dc20d3796e4e-1585491.jpg!d', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://c.pxhere.com/images/22/ac/1d5d2173b4dc18c4dc20d3796e4e-1585491.jpg!d', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para turma', 3, 'https://c.pxhere.com/images/22/ac/1d5d2173b4dc18c4dc20d3796e4e-1585491.jpg!d', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://c.pxhere.com/images/22/ac/1d5d2173b4dc18c4dc20d3796e4e-1585491.jpg!d', 1, null);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://c.pxhere.com/images/22/ac/1d5d2173b4dc18c4dc20d3796e4e-1585491.jpg!d', 1, 1);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://c.pxhere.com/images/22/ac/1d5d2173b4dc18c4dc20d3796e4e-1585491.jpg!d', 1, 2);
